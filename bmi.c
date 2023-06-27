811f5d938bc100d628ece3401f69f83209913327
Cafei-xx <cedriblinks@gmail.com>
2023-06-27 04:29:25 -0700

    simple code

--- bmi.c
+++ bmi.c
@@ -1,48 +1,17 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-
-#define BUFFER_SIZE 256
+#include <stdio.h>
 
 /**
- * main - Entry point of the program. Calculates Tina's Body Mass Index (BMI).
+ * main - Entry point of Tina program
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char weightPrompt[] = "Please enter your weight in kilograms (e.g., 68.5): ";
-	char heightPrompt[] = "Please enter your height in centimeters (e.g., 165): ";
-	char bmiMessage[] = "Tina, your Body Mass Index (BMI) is: ";
+	int number1 = 180;
+	int number2 = 200;
+	int sum = number1 + number2;
 
-	char weightStr[BUFFER_SIZE];
-	char heightStr[BUFFER_SIZE];
-	float weight, height, bmi;
-
-	write(STDOUT_FILENO, weightPrompt, sizeof(weightPrompt) - 1);
-	fflush(stdout);
-
-	ssize_t weightLength = read(STDIN_FILENO, weightStr, BUFFER_SIZE);
-	weightStr[weightLength - 1] = '\0'; /* Remove trailing newline */
-	weight = atof(weightStr);
-
-	write(STDOUT_FILENO, heightPrompt, sizeof(heightPrompt) - 1);
-	fflush(stdout);
-
-	ssize_t heightLength = read(STDIN_FILENO, heightStr, BUFFER_SIZE);
-	heightStr[heightLength - 1] = '\0'; /* Remove trailing newline */
-	height = atof(heightStr);
-
-	height /= 100;
-
-	bmi = weight / (height * height);
-
-	write(STDOUT_FILENO, bmiMessage, sizeof(bmiMessage) - 1);
-	char bmiStr[BUFFER_SIZE];
-	int bmiLength = snprintf(bmiStr, BUFFER_SIZE, "%.2f\n", bmi);
-	write(STDOUT_FILENO, bmiStr, bmiLength);
+	printf("The sum of %d and %d is: %d\n", number1, number2, sum);
 
 	return (0);
 }
